80e6d620b8930b1c6463cef3100cc9904f6c9dc2
STRIXBG <65187330+STRIXBG@users.noreply.github.com>
2023-04-10 13:39:54 +0300

    Update exercise.java

--- Java-Advanced-Softuni-main/Sets And Maps Advanced - Lab/Parking Lot/exercise.java
+++ Java-Advanced-Softuni-main/Sets And Maps Advanced - Lab/Parking Lot/exercise.java
@@ -1,32 +1,40 @@
-package com.softuni;
-
 import java.util.*;
 
+public class Main {
+    static Stack<String> carNumbers = new Stack<>();
 
-public class Exercise {
+    private static void parkCar(String number) {
+        carNumbers.add(number);
+    }
+
+    private static void removeCar(String number) {
+        if (carNumbers.size() != 0) {
+            carNumbers.remove(number);
+        }
+    }
+
+    private static void printAllCarNumbers() {
+        if (carNumbers.size() == 0) {
+            System.out.println("Parking Lot is Empty");
+        } else {
+            for (String number :
+                    carNumbers) {
+                System.out.println(number);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Set<String> parkedCars = new HashSet<>();
-        String input = scanner.nextLine();
-        while(!Objects.equals(input, "END")){
-            String[] inputInfo = input.split(", ");
-            String command = inputInfo[0];
-            String carNum = inputInfo[1];
-            if(Objects.equals(command, "IN")){
-                parkedCars.add(carNum);
-            }
-            else if(Objects.equals(command, "OUT")){
-                parkedCars.remove(carNum);
-            }
-            input = scanner.nextLine();
+        String line = scanner.nextLine();
+        while (!line.equals("END")) {
+            String[] splitLine = line.split(",");
+            String command = splitLine[0];
+            String carNumber = splitLine[1];
+            if (Objects.equals(command, "IN")) parkCar(carNumber);
+            else if (Objects.equals(command, "OUT")) removeCar(carNumber);
+            line = scanner.nextLine();
         }
-        if(!parkedCars.isEmpty()) {
-            for (String value : parkedCars) {
-                System.out.println(value);
-            }
-        }
-        else{
-            System.out.println("Parking Lot is Empty");
-        }
+        printAllCarNumbers();
     }
 }
