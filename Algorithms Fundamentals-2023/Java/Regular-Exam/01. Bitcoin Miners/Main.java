d2df8a11b9d7f2f5042964a5c5310f38b2f4e0aa
STRIXBG <65187330+STRIXBG@users.noreply.github.com>
2023-07-22 12:18:46 +0300

    Update Main.java

--- Algorithms Fundamentals-2023/Java/Regular-Exam/01. Bitcoin Miners/Main.java
+++ Algorithms Fundamentals-2023/Java/Regular-Exam/01. Bitcoin Miners/Main.java
@@ -1,19 +1,5 @@
 import java.util.Scanner;
 
-public class Solution {
-    // x <= n
-    // max_transactions > available_transactions
-    int solve(int n, int x) {
-        if (x == 0 || n == x) {
-            return 1;
-        }
-        if (x > n) {
-            return 0;
-        }
-        return solve(n - 1, x - 1) + solve(n - 1, x);
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
